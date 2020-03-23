9078105cb5d5319eca1a0de8f1fead1ed87c85f1
Evan Whelan <evan@batman.cgocable.net>
2020-03-23 12:38:46 -0400

    cumbox 2.0 RELEASE!

---
+++ install-arm.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo mkdir -p /opt/cumbox
+sudo chmod 777 /opt/cumbox
+wget https://assets.checkra.in/downloads/linux/cli/arm/c5cbb125c6948b39383702b62cec4f184263c8db50f49b9328013213126dae78/checkra1n -P /opt/cumbox
+sudo cp 01-cumbox.rules /etc/udev/rules.d
+echo "cumbox is now installed!"
