52221de4fdb79d7ca13d701b27182c2aa3ff2a67
THIS IS MY NAME <Lloir@users.noreply.github.com>
2020-09-23 14:59:12 +0100

    0.11.0 Beta

--- install-arm64.sh
+++ install-arm64.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo mkdir -p /opt/cumbox
 sudo chmod 777 /opt/cumbox
-wget https://assets.checkra.in/downloads/linux/cli/arm64/32cc7d1bd687800d1b5bc5cfc4f4ca15f393dada7570e78742eab068b1f2a3e2/checkra1n -O /opt/cumbox/checkra1n
+wget https://assets.checkra.in/downloads/linux/cli/arm64/b48774e5d240ce192016a3fa97df7ef855220576f0704c83ed627d092cb2e224/checkra1n -O /opt/cumbox/checkra1n
 sudo chmod +x /opt/cumbox/checkra1n
 sudo cp 01-cumbox.rules /etc/udev/rules.d
 echo "cumbox is now installed!"
